88838876cef4397aca965d6d99a4afc333ef1e17
csvillalta <christophervillalta@gmail.com>
2019-03-27 02:46:48 -0400

    Add state density and frequency plots

--- graphs.py
+++ graphs.py
@@ -2,26 +2,32 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-def discrete_state_density_plot(states, ax=None, title=None):
-    unique_states, counts = np.unique(states, axis=0, return_counts=True)
-    unique_states_x = unique_states[:, 0]
-    unique_states_y = unique_states[:, 1]
-    
-    goal = matplotlib.patches.Circle((0, 0), 1, fill=False, color='r', linewidth=1, linestyle='--')
-    normalize = matplotlib.colors.Normalize(vmin=0, vmax=200)
+def plot_state_density(states, ax=None, title=None):
     ax = ax or plt.gca()
-    if title: ax.set_title(title)
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
     ax.set_aspect('equal')
+    if title: ax.set_title(title)
+    goal = matplotlib.patches.Circle((0,0), 1, fill=False, color='r', linestyle='--')
     ax.add_artist(goal)
-    ax.scatter(unique_states_x, unique_states_y, c=counts, alpha=0.9, norm=normalize)
+    # hist = ax2.hist2d(states[:,0], states[:,1], bins=100, cmap='Reds')
+    ax.scatter(states[:,0], states[:,1], alpha=0.0075, marker='o', c='green')
     return ax
-
+    
+def plot_state_frequency(states, fig=None, ax=None, title=None):
+    unique_states, counts = np.unique(states, axis=0, return_counts=True)
+    fig = fig or plt.gcf()
+    ax = ax or plt.gca()
+    ax.set_aspect('equal')
+    if title: ax.set_title(title)
+    goal = matplotlib.patches.Circle((0,0), 1, fill=False, color='r', linestyle='--')
+    ax.add_artist(goal)
+    scatter = ax.scatter(unique_states[:,0], unique_states[:,1], c=counts, cmap='Reds', marker='.')
+    fig.colorbar(scatter, ax=ax)
+    return ax
+    
 def plot_states(states, ax=None, title=None):
     ax = ax or plt.gca()
-    if title: ax.set_title(title)
     ax.set_aspect('equal')
+    if title: ax.set_title(title)
     goal = matplotlib.patches.Circle((0, 0), 1, fill=False, color='r', linewidth=1, linestyle='--')
     ax.add_artist(goal)
     ax.scatter(states[:,0], states[:,1], c='b', marker='.') 
@@ -30,9 +36,8 @@ def plot_states(states, ax=None, title=None):
 def plot_state_influence(states, influence, fig=None, ax=None, title=None):
     fig = fig or plt.gcf()
     ax = ax or plt.gca()
-    
-    if title: ax.set_title(title)
     ax.set_aspect('equal')
+    if title: ax.set_title(title)
     goal = matplotlib.patches.Circle((0, 0), 1, fill=False, color='r', linewidth=1, linestyle='--')
     ax.add_artist(goal)
     scatter = ax.scatter(states[:,0], states[:,1], c=influence, marker='.')
